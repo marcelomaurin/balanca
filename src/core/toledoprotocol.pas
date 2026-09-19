6665dee227b6abb82d26d08db8b6dbbcb8d98eed
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-19 11:29:54 -0300

    feat: adiciona codificacao de comandos ao protocolo Toledo

--- src/core/toledoprotocol.pas
+++ src/core/toledoprotocol.pas
@@ -5,7 +5,7 @@ unit toledoprotocol;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, scalecommands;
 
 const
   TOLEDO_ENQ = #$05;
@@ -41,6 +41,8 @@ type
 
     procedure Reset;
     procedure Feed(const AData: string);
+    function SupportsCommand(ACommand: TScaleCommand): Boolean;
+    function TryEncodeCommand(ACommand: TScaleCommand; out AData: string): Boolean;
 
     property State: TToledoParserState read FState;
     property LastFrame: string read FLastFrame;
@@ -137,6 +139,28 @@ var
 begin
   for I := 1 to Length(AData) do
     ProcessByte(AData[I]);
+end;
+
+function TToledoProtocol.SupportsCommand(ACommand: TScaleCommand): Boolean;
+begin
+  // Neste projeto, somente a leitura via ENQ está confirmada para o
+  // equipamento Toledo/compatível. Outros comandos permanecem padronizados
+  // no núcleo, mas não são enviados sem confirmação do protocolo.
+  Result := ACommand = scReadWeight;
+end;
+
+function TToledoProtocol.TryEncodeCommand(ACommand: TScaleCommand;
+  out AData: string): Boolean;
+begin
+  AData := '';
+  Result := SupportsCommand(ACommand);
+
+  if not Result then
+    Exit;
+
+  case ACommand of
+    scReadWeight: AData := TOLEDO_ENQ;
+  end;
 end;
 
 end.
